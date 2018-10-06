6f5e12323d64dd09063fe5317960cc2e0b30ef2f
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-06 01:38:43 -0500

    video player embedded and working, able to change selected video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video, onVideoSelect}) => {
+  // const video = props.video;
+  // {video} completely identical to above. "Says the arguement, this first object
+  // in the arguements array has a property video, please grab that video and
+  // declare a new variable called video.
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+
+  return (
+    <li onClick={()=>{onVideoSelect(video)}} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -3,26 +3,37 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import { API_KEY } from '../config.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
       console.log(videos);
       this.render();
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={(video) => {this.setState({selectedVideo : video})}}
+          />
       </div>
     );
   };
